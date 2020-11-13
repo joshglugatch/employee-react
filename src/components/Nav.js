effd2cadcf2673573a84f77b85a396c5cd16e159
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 17:54:12 -0800

    create nav

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return(
+    <div className="nav justify-content-center">
+      <h1 className="pageTitle">Employee Tracker with React.js</h1>
+    </div>
+  )
+};
+
+export default Navbar;
