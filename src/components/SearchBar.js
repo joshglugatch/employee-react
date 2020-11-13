4b74a4e262deca5b956f03345a65be606ea6b95c
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 17:54:20 -0800

    create searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+  <div className="searchDiv">
+    <input type="text" className="search"/>
+  </div>
+  )
+};
+
+export default SearchBar;
