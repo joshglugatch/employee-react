ff9f5653d2598489118de96c0484deac30d3eb93
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 20:44:52 -0800

    Change API to JSON (wasn't working properly)

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-function getEmployee(){
-    return axios.get(
-      "https://randomuser.me/api/?inc=picture,name,dob,phone,email&results=20"
-    );
-};
-
-export default getEmployee;
