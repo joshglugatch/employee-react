b04e469f43af31cc1467773dc7019915337aec74
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 17:55:56 -0800

    create function component for App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from "react";
+import Page from "./pages/Page";
+import getEmployee from "./utils/API";
+import EmployeeContext from "./utils/EmployeeContext";
+import "./App.css"
+
 
 function App() {
+
+  const [picture, setPicture] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [age, setAge] = useState("")
+   
+
+useEffect(()=>{
+  getEmployee().then(res=>{
+    console.log(res.data.results[0])
+    setPicture(res.data.results[0].picture.thumbnail)
+    setName(res.data.results[0].name.first +" "+ res.data.results[0].name.last)
+    setEmail(res.data.results[0].email)
+    setPhone(res.data.results[0].phone)
+    setAge(res.data.results[0].dob.age)
+  })
+},[])
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EmployeeContext.Provider value={{picture,name,email,phone,age}}>
+      <Page />
+    </EmployeeContext.Provider>
   );
 }
+
 
 export default App;
