70bbb564c15089316663e46aaeae97f63d0ad091
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 20:45:07 -0800

    update app for JSON instead of API

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { useState,useEffect } from "react";
 import Page from "./pages/Page";
-import getEmployee from "./utils/API";
+import Employees from "./employees.json"
 import EmployeeContext from "./utils/EmployeeContext";
 import "./App.css"
 
 
 function App() {
-
-  const [picture, setPicture] = useState("")
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [phone, setPhone] = useState("")
-  const [age, setAge] = useState("")
-   
+  const [employeeData, setEmployeeData] = useState([])
+  
 
 useEffect(()=>{
-  getEmployee().then(res=>{
-    console.log(res.data.results[0])
-    setPicture(res.data.results[0].picture.thumbnail)
-    setName(res.data.results[0].name.first +" "+ res.data.results[0].name.last)
-    setEmail(res.data.results[0].email)
-    setPhone(res.data.results[0].phone)
-    setAge(res.data.results[0].dob.age)
-  })
+    
+    setEmployeeData(Employees)
+    //console.log(employeeData)
 },[])
+
   
- 
+  
+
+
+
   return (
-    <EmployeeContext.Provider value={{picture,name,email,phone,age}}>
+    <EmployeeContext.Provider value={{employeeData}}>
       <Page />
     </EmployeeContext.Provider>
   );
