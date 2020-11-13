8c34c7d8815f879ee8b31ec8a4de1dd965e7ea0e
Josh Glugatch <joshglugatch@gmail.com>
2020-11-12 22:24:28 -0800

    create search context

---
+++ src/utils/SearchContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+// default context object with properties corresponding to Provider values
+
+const SearchContext = React.createContext({
+  search:""
+  
+});
+
+export default SearchContext;
