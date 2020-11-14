8b34b7fb9c046262d5762bf93b058fc9b0ac4cfb
Josh Glugatch <joshglugatch@gmail.com>
2020-11-13 16:30:57 -0800

    add sort functions

--- src/App.js
+++ src/App.js
@@ -7,18 +7,47 @@ import SearchContext from "./utils/SearchContext";
 
 
 function App() {
+  //create states for employee array and search value
   const [employeeData, setEmployeeData] = useState([])
   const [search, setSearch] = useState("")
   
+  //Grab search value on change
   const  handleSearch = (e)=>{
-    setSearch({search: e.target.value})
+    setSearch(e.target.value)
   }
   
+  //Sort alphabetically ascending on button click
+  const handleSortA = (e)=>{
+    e.preventDefault();
+    var sorted = employeeData.sort(function(a,b){
+      if(a.name.toLowerCase() < b.name.toLowerCase()) { return -1; }
+      if(a.name.toLowerCase() > b.name.toLowerCase()) { return 1; }
+      return 0;
+    })
+    
+    setEmployeeData([...sorted])
+  }
   
-  
+  //Sort alphabetically descending on button click
+  const handleSortZ = (e)=>{
+    e.preventDefault();
+    var sorted = employeeData.sort(function(a,b){
+      if(b.name.toLowerCase() < a.name.toLowerCase()) { return -1; }
+      if(b.name.toLowerCase() > a.name.toLowerCase()) { return 1; }
+      return 0;
+    })
+    
+    setEmployeeData([...sorted])
+  }
 
+  //render data on each change of search
 useEffect(()=>{
-setEmployeeData(Employees)
+  if(search === ""){
+    setEmployeeData(Employees)
+  }else{
+    setEmployeeData(Employees.filter(item=>item.name.toLowerCase().includes(search.toLowerCase())))
+  }
+
 console.log(search)
 
 },[search])
@@ -26,10 +55,10 @@ console.log(search)
   
   
 
-
+//provide values for search/sort functions and employee data to page
 
   return (
-    <SearchContext.Provider value={{search, handleSearch}}>
+    <SearchContext.Provider value={{search, handleSearch, handleSortA, handleSortZ}}>
       <EmployeeContext.Provider value={{employeeData}}>
         <Page />
       </EmployeeContext.Provider>
